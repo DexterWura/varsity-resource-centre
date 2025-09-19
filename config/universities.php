caef0f2ae00ddada457e0b86aec0c857e039496b
DexterWura <dexterwurayayi@gmail.com>
2025-09-19 11:06:59 +0200

    use CSV export URLs for MSU sheets (faculties/modules/timetable)

--- config/universities.php
+++ config/universities.php
@@ -11,9 +11,9 @@ return [
     // Example university entries. Replace the CSV URLs with your published sheet links.
     'msu' => [
         'name' => 'Midlands State University',
-        'faculties_url' => 'https://docs.google.com/spreadsheets/d/EXAMPLE_ID/export?format=csv&gid=FACULTIES_GID',
-        'modules_url' => 'https://docs.google.com/spreadsheets/d/EXAMPLE_ID/export?format=csv&gid=MODULES_GID',
-        'timetable_url' => 'https://docs.google.com/spreadsheets/d/EXAMPLE_ID/export?format=csv&gid=TIMETABLE_GID',
+        'faculties_url' => 'https://docs.google.com/spreadsheets/d/1k4UF32H7jf9q3FviCb84CiINNJe79PbvxLt7ulKSjIE/export?format=csv&gid=0',
+        'modules_url' => 'https://docs.google.com/spreadsheets/d/1k4UF32H7jf9q3FviCb84CiINNJe79PbvxLt7ulKSjIE/export?format=csv&gid=1395780808',
+        'timetable_url' => 'https://docs.google.com/spreadsheets/d/1k4UF32H7jf9q3FviCb84CiINNJe79PbvxLt7ulKSjIE/export?format=csv&gid=279139838',
     ],
     // Add more universities below
     'uz' => [
